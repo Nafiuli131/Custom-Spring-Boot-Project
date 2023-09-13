eb05a4fc341df0cf721dd650dff9634718496a76
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-09-13 15:59:30 +0600

    add global exception handling

--- spring_project/src/main/java/com/example/spring_project/generics/GenericController.java
+++ spring_project/src/main/java/com/example/spring_project/generics/GenericController.java
@@ -1,11 +1,11 @@
 package com.example.spring_project.generics;
 
+import com.example.spring_project.exception.ResourceNotFoundExceptionHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Objects;
 import java.util.Optional;
 
 
@@ -13,60 +13,36 @@ public class GenericController<T extends BaseEntity> {
     @Autowired
     private GenericService<T> genericService;
 
+    private final String OBJECT_NOT_FOUND = "No Object Found";
+
     @PostMapping
-    public ResponseEntity<Object> save(@RequestBody T entity) {
-        try {
-            return new ResponseEntity<>(genericService.save(entity), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Error to save!", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<Object> save(@RequestBody T entity) throws Exception {
+        return new ResponseEntity<>(genericService.save(entity), HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<Object> update(@RequestBody T entity) {
-        try {
-            Optional<T> getObject = genericService.findById(entity.getId());
-            if(getObject.isPresent()){
-                return new ResponseEntity<>(genericService.update(entity), HttpStatus.OK);
-            }else{
-                return new ResponseEntity<>("No object found", HttpStatus.OK) ;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Error to save!", HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Object> update(@RequestBody T entity) throws Exception {
+        Optional<T> getObject = genericService.findById(entity.getId());
+        if (getObject.isPresent()) {
+            return new ResponseEntity<>(genericService.update(entity), HttpStatus.OK);
+        } else {
+            throw new ResourceNotFoundExceptionHandler(OBJECT_NOT_FOUND);
         }
     }
 
     @GetMapping
-    public ResponseEntity<?> findAll() {
-        try {
-            return new ResponseEntity<>(genericService.findAll(), HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Error to get all objects!", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<?> findAll() throws Exception {
+        return new ResponseEntity<>(genericService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable("id") Long id){
-        try {
-            return new ResponseEntity<>(genericService.findById(id),HttpStatus.OK);
-        }catch (Exception e){
-            e.printStackTrace();
-            return new ResponseEntity<>("Error to find this object!", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<?> findById(@PathVariable("id") Long id) throws Exception {
+        return new ResponseEntity<>(genericService.findById(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable("id") Long id) {
-        try {
-            genericService.deleteById(id);
-            return new ResponseEntity<>("Delete Successfully!", HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Error to delete!", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<String> delete(@PathVariable("id") Long id) throws Exception {
+        genericService.deleteById(id);
+        return new ResponseEntity<>("Delete Successfully!", HttpStatus.OK);
     }
 }

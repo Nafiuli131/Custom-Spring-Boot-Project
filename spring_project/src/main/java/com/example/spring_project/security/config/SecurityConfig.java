ccec1650b3685cd0ab544c10fd3def008e389904
Nafiul-Islam-Nayeem <nafiul.islam@digigate360.com>
2023-09-18 17:58:37 +0600

    fix jwt

--- spring_project/src/main/java/com/example/spring_project/security/config/SecurityConfig.java
+++ spring_project/src/main/java/com/example/spring_project/security/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
                 .authorizeRequests() .antMatchers("/auth/**").permitAll()
-//                .antMatchers("/employee/**").hasAnyRole("admin")
+                .antMatchers("/employee/**").hasAnyRole("admin")
                 .anyRequest().authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
